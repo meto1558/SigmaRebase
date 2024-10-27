b045f47bed2cbe7d0c39d39f87e41ba1c0330415
Mark <86848614+MarkGG8181@users.noreply.github.com>
2024-10-27 10:58:41 +0200

    fix cape module

--- src/java/com/mentalfrostbyte/jello/module/impl/player/Cape.java
+++ src/java/com/mentalfrostbyte/jello/module/impl/player/Cape.java
@@ -1,10 +1,12 @@
 package com.mentalfrostbyte.jello.module.impl.player;
 
+import com.mentalfrostbyte.jello.events.impl.TickEvent;
 import com.mentalfrostbyte.jello.module.Module;
 import com.mentalfrostbyte.jello.module.ModuleCategory;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.player.AbstractClientPlayerEntity;
 import net.minecraft.util.ResourceLocation;
+import team.sdhq.eventBus.annotations.EventTarget;
 
 public class Cape extends Module {
 
@@ -15,11 +17,19 @@ public class Cape extends Module {
     }
 
     public ResourceLocation getCape() {
-        return new ResourceLocation("com/mentalfrostbyte/gui/resources/jello/capes/cape.png");
+        return new ResourceLocation("minecraft", "com/mentalfrostbyte/gui/resources/jello/capes/cape.png");
     }
 
-    public boolean canRender(AbstractClientPlayerEntity player) {
-        return player == mc.player || isEnabled();
+    public void onDisable() {
+        if (mc.player != null) {
+            mc.player.setLocationOfCape(null);
+        }
+    }
+
+    @EventTarget
+    public void onTick(TickEvent event) {
+        if (mc.player != null) {
+            mc.player.setLocationOfCape(getCape());
+        }
     }
 }
-//
