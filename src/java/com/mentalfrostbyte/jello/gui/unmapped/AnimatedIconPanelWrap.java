ee73b74959ebe0bc8d139df46740ae3013e5d491
DataM0del <183248792+DataM0del@users.noreply.github.com>
2024-12-16 23:14:17 -0600

    refactor(AnimatedIconPanelWrap): rename all CustomGuiScreen params to `screen`

--- src/java/com/mentalfrostbyte/jello/gui/unmapped/AnimatedIconPanelWrap.java
+++ src/java/com/mentalfrostbyte/jello/gui/unmapped/AnimatedIconPanelWrap.java
@@ -10,19 +10,19 @@ import com.mentalfrostbyte.jello.util.unmapped.ClientResource;
 // as it just passes in the arguments to it and there's no extra methods
 public class AnimatedIconPanelWrap extends AnimatedIconPanel {
 
-    public AnimatedIconPanelWrap(CustomGuiScreen var1, String var2, int var3, int var4, int var5, int var6, boolean var7) {
-      super(var1, var2, var3, var4, var5, var6, var7);
+    public AnimatedIconPanelWrap(CustomGuiScreen screen, String var2, int var3, int var4, int var5, int var6, boolean var7) {
+      super(screen, var2, var3, var4, var5, var6, var7);
    }
 
-   public AnimatedIconPanelWrap(CustomGuiScreen var1, String var2, int var3, int var4, int var5, int var6, ColorHelper var7, boolean var8) {
-      super(var1, var2, var3, var4, var5, var6, var7, var8);
+   public AnimatedIconPanelWrap(CustomGuiScreen screen, String var2, int var3, int var4, int var5, int var6, ColorHelper var7, boolean var8) {
+      super(screen, var2, var3, var4, var5, var6, var7, var8);
    }
 
-   public AnimatedIconPanelWrap(CustomGuiScreen var1, String var2, int var3, int var4, int var5, int var6, ColorHelper var7, String var8, boolean var9) {
-      super(var1, var2, var3, var4, var5, var6, var7, var8, var9);
+   public AnimatedIconPanelWrap(CustomGuiScreen screen, String var2, int var3, int var4, int var5, int var6, ColorHelper var7, String var8, boolean var9) {
+      super(screen, var2, var3, var4, var5, var6, var7, var8, var9);
    }
 
-   public AnimatedIconPanelWrap(CustomGuiScreen var1, String var2, int var3, int var4, int var5, int var6, ColorHelper var7, String var8, ClientResource var9, boolean var10) {
-      super(var1, var2, var3, var4, var5, var6, var7, var8, var9, var10);
+   public AnimatedIconPanelWrap(CustomGuiScreen screen, String var2, int var3, int var4, int var5, int var6, ColorHelper var7, String var8, ClientResource var9, boolean var10) {
+      super(screen, var2, var3, var4, var5, var6, var7, var8, var9, var10);
    }
 }
