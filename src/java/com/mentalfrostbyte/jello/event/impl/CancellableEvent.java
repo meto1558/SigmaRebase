e6ee998a9616316b17b85ad98fa47002175e72f4
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-01-18 13:53:32 -0600

    refactor(Class4406): rename to `CancellableEvent`

--- src/java/com/mentalfrostbyte/jello/event/impl/CancellableEvent.java
+++ src/java/com/mentalfrostbyte/jello/event/impl/CancellableEvent.java
@@ -4,16 +4,16 @@ package com.mentalfrostbyte.jello.event.impl;
 import com.mentalfrostbyte.jello.event.Event;
 import com.mentalfrostbyte.jello.event.ICancellableEvent;
 
-public class Class4406 implements Event, ICancellableEvent {
-    public boolean field21520 = false;
+public class CancellableEvent implements Event, ICancellableEvent {
+    public boolean cancelled = false;
 
     @Override
     public boolean setCancelled(boolean var1) {
-        return this.field21520 = var1;
+        return this.cancelled = var1;
     }
 
     @Override
     public boolean isCancelled() {
-        return this.field21520;
+        return this.cancelled;
     }
 }
