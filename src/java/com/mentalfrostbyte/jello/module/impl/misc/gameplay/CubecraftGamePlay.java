713361cdd9f4ceafdf2bed657d3bc8db2694d412
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-01-22 21:51:45 +0200

    remap GamePlay module

--- src/java/com/mentalfrostbyte/jello/module/impl/misc/gameplay/CubecraftGamePlay.java
+++ src/java/com/mentalfrostbyte/jello/module/impl/misc/gameplay/CubecraftGamePlay.java
@@ -4,7 +4,7 @@ package com.mentalfrostbyte.jello.module.impl.misc.gameplay;
 import com.mentalfrostbyte.Client;
 import com.mentalfrostbyte.jello.event.impl.ReceivePacketEvent;
 import com.mentalfrostbyte.jello.managers.impl.notifs.Notification;
-import com.mentalfrostbyte.jello.misc.Class7200;
+import com.mentalfrostbyte.jello.misc.TimedMessage;
 import com.mentalfrostbyte.jello.module.Module;
 import com.mentalfrostbyte.jello.module.ModuleCategory;
 import com.mentalfrostbyte.jello.module.impl.misc.GamePlay;
@@ -12,11 +12,10 @@ import net.minecraft.network.IPacket;
 import net.minecraft.network.play.server.SChatPacket;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.event.ClickEvent;
-import net.minecraft.util.text.event.ClickEventAction;
 import team.sdhq.eventBus.annotations.EventTarget;
 
 public class CubecraftGamePlay extends Module {
-    public GamePlay field23580;
+    public GamePlay parentModule;
 
     public CubecraftGamePlay() {
         super(ModuleCategory.MISC, "Cubecraft", "Gameplay for Cubecraft");
@@ -24,49 +23,47 @@ public class CubecraftGamePlay extends Module {
 
     @Override
     public void initialize() {
-        this.field23580 = (GamePlay) this.access();
+        this.parentModule = (GamePlay) this.access();
     }
 
     @EventTarget
-    public void method16296(ReceivePacketEvent var1) {
+    public void onPacket(ReceivePacketEvent event) {
         if (this.isEnabled() && mc.player != null) {
-            IPacket var4 = var1.getPacket();
-            if (var4 instanceof SChatPacket) {
-                SChatPacket var5 = (SChatPacket) var4;
-                String var6 = var5.getChatComponent().getString();
-                String var7 = var5.getChatComponent().getString();
-                String var8 = mc.player.getName().getString().toLowerCase();
-                if (this.field23580.getBooleanValueFromSettingName("AutoL")
+            IPacket<?> packet = event.getPacket();
+            if (packet instanceof SChatPacket chatPacket) {
+                String text = chatPacket.getChatComponent().getString();
+                String playerName = mc.player.getName().getString().toLowerCase();
+                if (this.parentModule.getBooleanValueFromSettingName("AutoL")
                         && (
-                        var6.toLowerCase().contains("was slain by " + var8)
-                                || var6.toLowerCase().contains("burned to death while fighting " + var8)
-                                || var6.toLowerCase().contains("was shot by " + var8)
-                                || var6.toLowerCase().contains("burnt to a crisp while fighting " + var8)
-                                || var6.toLowerCase().contains("couldn't fly while escaping " + var8)
-                                || var6.toLowerCase().contains("thought they could survive in the void while escaping " + var8)
-                                || var6.toLowerCase().contains("fell to their death while escaping " + var8)
-                                || var6.toLowerCase().contains("died in the void while escaping " + var8)
+                        text.toLowerCase().contains("was slain by " + playerName)
+                                || text.toLowerCase().contains("burned to death while fighting " + playerName)
+                                || text.toLowerCase().contains("was shot by " + playerName)
+                                || text.toLowerCase().contains("burnt to a crisp while fighting " + playerName)
+                                || text.toLowerCase().contains("couldn't fly while escaping " + playerName)
+                                || text.toLowerCase().contains("thought they could survive in the void while escaping " + playerName)
+                                || text.toLowerCase().contains("fell to their death while escaping " + playerName)
+                                || text.toLowerCase().contains("died in the void while escaping " + playerName)
                 )) {
-                    this.field23580.method16761(var6);
+                    this.parentModule.processAutoLMessage(text);
                 }
 
-                if (var7.contains("§a§lPlay Again §r§8• §r§6§lAuto Mode §r§8• §r§c§lLeave") && this.field23580.getBooleanValueFromSettingName("Auto Join")) {
-                    for (ITextComponent var10 : var5.getChatComponent().getSiblings()) {
+                if (text.contains("§a§lPlay Again §r§8• §r§6§lAuto Mode §r§8• §r§c§lLeave") && this.parentModule.getBooleanValueFromSettingName("Auto Join")) {
+                    for (ITextComponent var10 : chatPacket.getChatComponent().getSiblings()) {
                         ClickEvent var11 = var10.getStyle().getClickEvent();
                         if (var11 != null && var11.getAction() == ClickEvent.Action.RUN_COMMAND && var11.getValue().contains("playagain")) {}
-                            this.field23580.method16759(new Class7200(var11.getValue(), (long) this.field23580.getNumberValueBySettingName("Auto Join delay") * 1000L));
+                            this.parentModule.updateTimedMessage(new TimedMessage(var11.getValue(), (long) this.parentModule.getNumberValueBySettingName("Auto Join delay") * 1000L));
                             Client.getInstance()
                                     .notificationManager
                                     .send(
-                                            new Notification("Auto Join", "Joining a new game in 3 seconds.", (int) (this.field23580.getNumberValueBySettingName("Auto Join delay") - 1.0F) * 1000)
+                                            new Notification("Auto Join", "Joining a new game in 3 seconds.", (int) (this.parentModule.getNumberValueBySettingName("Auto Join delay") - 1.0F) * 1000)
                                     );
                             break;
                         }
                     }
 
 
-                if (this.field23580.getBooleanValueFromSettingName("AutoGG") && var7.equalsIgnoreCase("§e" + mc.player.getName().getString() + "§r§a won the game!§r")) {
-                    this.field23580.method16760();
+                if (this.parentModule.getBooleanValueFromSettingName("AutoGG") && text.equalsIgnoreCase("§e" + mc.player.getName().getString() + "§r§a won the game!§r")) {
+                    this.parentModule.initializeAutoL();
                 }
             }
         }
