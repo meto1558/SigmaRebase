6b964bfd1c19080faef798ea5b73453d0612644f
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-01-23 20:40:47 +0200

    playSong

--- src/java/com/mentalfrostbyte/jello/gui/impl/SearchBoxButton.java
+++ src/java/com/mentalfrostbyte/jello/gui/impl/SearchBoxButton.java
@@ -66,7 +66,7 @@ public class SearchBoxButton extends AnimatedIconPanelWrap {
                                                                     var4
                                                             )
                                                     );
-                                            var7x.doThis((var2, var3xx) -> this.field20843.method24317(null, var4));
+                                            var7x.doThis((var2, var3xx) -> this.field20843.playSong(null, var4));
                                         }
                                     }
                                 }
