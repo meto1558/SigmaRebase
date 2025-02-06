ec1c753c07f15ac30c9205c76ab2bce234cf7ab6
StormingMoon <stormingmoon@proton.me>
2025-02-06 23:00:25 +0100

    shit no work

--- src/main/java/com/mentalfrostbyte/jello/util/game/player/MovementUtil2.java
+++ src/main/java/com/mentalfrostbyte/jello/util/game/player/MovementUtil2.java
@@ -319,7 +319,7 @@ public class MovementUtil2 {
 
     public static void block() {
         mc.getConnection().sendPacket(new CPlayerTryUseItemPacket(Hand.MAIN_HAND));
-        //mc.getConnection().sendPacket(new CPlayerTryUseItemPacket(Hand.OFF_HAND));
+        mc.getConnection().sendPacket(new CPlayerTryUseItemPacket(Hand.OFF_HAND));
     }
     public static void unblock() {
         mc.getConnection().sendPacket(new CPlayerDiggingPacket(CPlayerDiggingPacket.Action.RELEASE_USE_ITEM, new BlockPos(0, 0, 0), Direction.DOWN));
