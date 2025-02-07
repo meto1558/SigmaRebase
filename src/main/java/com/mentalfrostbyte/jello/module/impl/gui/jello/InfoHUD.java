6d20ba99776297bb62a42b3911206f9563a7c251
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-02-07 22:19:42 +0200

    remap InfoHUD

--- src/main/java/com/mentalfrostbyte/jello/module/impl/gui/jello/InfoHUD.java
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/gui/jello/InfoHUD.java
@@ -1,25 +1,23 @@
 package com.mentalfrostbyte.jello.module.impl.gui.jello;
 
-
 import com.mentalfrostbyte.jello.event.impl.game.render.EventRender2DOffset;
 import com.mentalfrostbyte.jello.module.Module;
 import com.mentalfrostbyte.jello.module.ModuleCategory;
 import com.mentalfrostbyte.jello.module.settings.impl.BooleanSetting;
 import com.mentalfrostbyte.jello.module.settings.impl.ModeSetting;
 import com.mentalfrostbyte.jello.util.client.ClientColors;
-import com.mentalfrostbyte.jello.util.game.player.MovementUtil2;
-
 import com.mentalfrostbyte.jello.util.client.render.ResourceRegistry;
+import com.mentalfrostbyte.jello.util.game.player.MovementUtil2;
 import com.mentalfrostbyte.jello.util.game.render.RenderUtil;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.screen.ChatScreen;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.AirItem;
+import net.minecraft.item.ItemStack;
 import org.lwjgl.opengl.GL11;
 import team.sdhq.eventBus.annotations.EventTarget;
 
 public class InfoHUD extends Module {
-    public float field23851 = 0.0F;
+    public float previousYaw = 0.0F;
 
     public InfoHUD() {
         super(ModuleCategory.GUI, "Info HUD", "Shows a bunch of usefull stuff");
@@ -30,86 +28,81 @@ public class InfoHUD extends Module {
     }
 
     @EventTarget
-    public void method16692(EventRender2DOffset var1) {
+    public void onRender2D(EventRender2DOffset event) {
         if (this.isEnabled() && mc.player != null) {
             if (!Minecraft.getInstance().gameSettings.hideGUI) {
                 if (!(mc.currentScreen instanceof ChatScreen)) {
-                    float var4 = mc.player.rotationYaw % 360.0F - this.field23851 % 360.0F;
-                    this.field23851 = this.field23851 + var4 / (float) Minecraft.getFps() * 1.5F;
-                    boolean var5 = false;
-                    int var6 = 14;
+                    float yawDifference = mc.player.rotationYaw % 360.0F - this.previousYaw % 360.0F;
+                    this.previousYaw += yawDifference / (float) Minecraft.getFps() * 1.5F;
+                    int yOffset = 14;
+
                     if (this.getBooleanValueFromSettingName("Show Player")) {
-                        var6 += this.method16696(0, mc.mainWindow.getHeight() - 23, 114);
+                        yOffset += this.renderPlayerModel(0, mc.mainWindow.getHeight() - 23, 114);
                     }
 
                     if (this.getBooleanValueFromSettingName("Show Armor")) {
-                        var6 += this.method16695(var6, mc.mainWindow.getHeight() - 14) + 10;
+                        yOffset += this.renderArmorStatus(yOffset, mc.mainWindow.getHeight() - 14) + 10;
                     }
 
-                    if (!this.getStringSettingValueByName("Cords").equals("None")) {
-                        var6 += this.method16694(var6, 42) + 10;
+                    if (!this.getStringSettingValueByName("Coordinates").equals("None")) {
+                        yOffset += this.renderCoordinates(yOffset, 42) + 10;
                     }
                 }
             }
         }
     }
 
-    public String method16693(boolean var1) {
-        return !var1
-                ? Math.round(mc.player.getPosX())
-                        + " "
-                        + Math.round(mc.player.getPosY())
-                        + " "
-                        + Math.round(mc.player.getPosZ())
-                : (float) Math.round(mc.player.getPosX() * 10.0) / 10.0F
-                        + " "
-                        + (float) Math.round(mc.player.getPosY() * 10.0) / 10.0F
-                        + " "
-                        + (float) Math.round(mc.player.getPosZ() * 10.0) / 10.0F;
+    public String getFormattedCoordinates(boolean precise) {
+        return !precise
+                ? Math.round(mc.player.getPosX()) + " " +
+                Math.round(mc.player.getPosY()) + " " +
+                Math.round(mc.player.getPosZ())
+                : (float) Math.round(mc.player.getPosX() * 10.0) / 10.0F + " " +
+                (float) Math.round(mc.player.getPosY() * 10.0) / 10.0F + " " +
+                (float) Math.round(mc.player.getPosZ() * 10.0) / 10.0F;
     }
 
-    public int method16694(int var1, int var2) {
-        String var5 = "Facing South";
-        String var6 = this.method16693(this.getStringSettingValueByName("Cords").equals("Precise"));
+    public int renderCoordinates(int x, int yOffset) {
+        String direction = "Facing South";
+        String coordinates = this.getFormattedCoordinates(this.getStringSettingValueByName("Coordinates").equals("Precise"));
         RenderUtil.drawString(
                 ResourceRegistry.JelloMediumFont20,
-                (float) var1,
-                (float) (mc.mainWindow.getHeight() - var2),
-                var6,
+                (float) x,
+                (float) (mc.mainWindow.getHeight() - yOffset),
+                coordinates,
                 MovementUtil2.applyAlpha(ClientColors.LIGHT_GREYISH_BLUE.getColor(), 0.8F));
-        return Math.max(ResourceRegistry.JelloLightFont20.getWidth(var5),
-                ResourceRegistry.JelloMediumFont20.getWidth(var6));
+        return Math.max(ResourceRegistry.JelloLightFont20.getWidth(direction),
+                ResourceRegistry.JelloMediumFont20.getWidth(coordinates));
     }
 
-    public int method16695(int var1, int var2) {
-        int var5 = 0;
+    public int renderArmorStatus(int x, int y) {
+        int armorCount = 0;
 
-        for (int var6 = 0; var6 < mc.player.inventory.armorInventory.size(); var6++) {
-            ItemStack var7 = mc.player.inventory.armorInventory.get(var6);
-            if (!(var7.getItem() instanceof AirItem)) {
-                var5++;
-                int var8 = var2 - 32 * var5;
-                RenderUtil.method11479(var7, var1, var8, 32, 32);
-                GL11.glDisable(2896);
-                float var9 = 1.0F - (float) var7.getDamage() / (float) var7.getMaxDamage();
-                if (var9 != 1.0F) {
-                    RenderUtil.renderBackgroundBox((float) (var1 + 2), (float) (var8 + 28), 28.0F, 5.0F,
+        for (int i = 0; i < mc.player.inventory.armorInventory.size(); i++) {
+            ItemStack armorPiece = mc.player.inventory.armorInventory.get(i);
+            if (!(armorPiece.getItem() instanceof AirItem)) {
+                armorCount++;
+                int armorY = y - 32 * armorCount;
+                RenderUtil.renderItem(armorPiece, x, armorY, 32, 32);
+                GL11.glDisable(GL11.GL_LIGHTING);
+                float durability = 1.0F - (float) armorPiece.getDamage() / (float) armorPiece.getMaxDamage();
+                if (durability != 1.0F) {
+                    RenderUtil.renderBackgroundBox((float) (x + 2), (float) (armorY + 28), 28.0F, 5.0F,
                             MovementUtil2.applyAlpha(ClientColors.DEEP_TEAL.getColor(), 0.5F));
                     RenderUtil.renderBackgroundBox(
-                            (float) (var1 + 2),
-                            (float) (var8 + 28),
-                            28.0F * var9,
+                            (float) (x + 2),
+                            (float) (armorY + 28),
+                            28.0F * durability,
                             3.0F,
-                            MovementUtil2.applyAlpha(!((double) var9 <= 0.2) ? ClientColors.DARK_SLATE_GREY.getColor()
+                            MovementUtil2.applyAlpha(durability > 0.2 ? ClientColors.DARK_SLATE_GREY.getColor()
                                     : ClientColors.PALE_YELLOW.getColor(), 0.9F));
                 }
             }
         }
-
-        return var5 != 0 ? 32 : -7;
+        return armorCount != 0 ? 32 : -7;
     }
 
-    public int method16696(int var1, int var2, int var3) {
-        return var3 - 24;
+    public int renderPlayerModel(int x, int y, int height) {
+        return height - 24;
     }
 }
