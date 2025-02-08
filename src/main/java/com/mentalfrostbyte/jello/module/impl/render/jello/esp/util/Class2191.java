2c0d42b5d74097c9cf461e16e3bca0f1199fe1dd
Away <125528637+AwayXD@users.noreply.github.com>
2025-02-08 16:03:12 -0600

    report(Class2191)

---
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/render/jello/esp/util/Class2191.java
@@ -0,0 +1,9 @@
+package com.mentalfrostbyte.jello.module.impl.render.jello.esp.util;
+
+public enum Class2191 {
+    field14327,
+    field14328,
+    field14329;
+
+    private static final Class2191[] field14330 = new Class2191[]{field14327, field14328, field14329};
+}
