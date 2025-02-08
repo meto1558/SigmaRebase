1c8013bcba4b709dca69413b677c94d37a1d50c1
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-02-08 12:33:33 +0200

    Revert "moved classes"
    This reverts commit a55758a59bb38165462b11bdc4ef2ca7ded9db26.

--- src/main/java/com/mentalfrostbyte/jello/gui/base/interfaces/IHandler.java
+++
@@ -1,9 +0,0 @@
-package com.mentalfrostbyte.jello.gui.base.interfaces;
-
-import com.mentalfrostbyte.jello.gui.base.elements.Element;
-
-// Naming reason: `callUIHandlers` in `UIBase` calls everything in `uiHandlers` (which is a list of `UIHandler`s)
-// and passes itself as the argument
-public interface IHandler {
-   void handle(Element element);
-}
--- src/main/java/com/mentalfrostbyte/jello/gui/base/interfaces/IWidthSetter.java
+++
@@ -1,7 +0,0 @@
-package com.mentalfrostbyte.jello.gui.base.interfaces;
-
-import com.mentalfrostbyte.jello.gui.impl.CustomGuiScreen;
-
-public interface IWidthSetter {
-   void setWidth(CustomGuiScreen forScreen, CustomGuiScreen fromWidthOfThisScreen);
-}
