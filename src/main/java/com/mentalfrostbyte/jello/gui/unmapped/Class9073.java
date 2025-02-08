9f772b35663b151a987581ccf905f4d7d694e787
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-02-08 12:28:56 +0200

    removed classes

--- src/main/java/com/mentalfrostbyte/jello/gui/unmapped/Class9073.java
+++
@@ -1,8 +0,0 @@
-package com.mentalfrostbyte.jello.gui.unmapped;
-
-import com.mentalfrostbyte.jello.gui.impl.jello.ingame.panels.WaypointPanel;
-import net.minecraft.util.math.vector.Vector3i;
-
-public interface Class9073 {
-   void method33814(WaypointPanel var1, String var2, Vector3i var3, int var4);
-}
