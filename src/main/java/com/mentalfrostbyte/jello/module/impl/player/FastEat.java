d034a7e0b6652759a3bd18e412f4ef380289e20c
Away <125528637+AwayXD@users.noreply.github.com>
2025-02-08 19:37:44 -0600

    update(FastEat)

--- src/main/java/com/mentalfrostbyte/jello/module/impl/player/FastEat.java
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/player/FastEat.java
@@ -1,9 +1,12 @@
 package com.mentalfrostbyte.jello.module.impl.player;
 
+import com.mentalfrostbyte.Client;
 import com.mentalfrostbyte.jello.event.impl.player.EventPlayerTick;
 import com.mentalfrostbyte.jello.module.Module;
 import com.mentalfrostbyte.jello.module.ModuleCategory;
 
+import com.mentalfrostbyte.jello.module.impl.movement.BlockFly;
+import com.mentalfrostbyte.jello.module.impl.world.Timer;
 import com.mentalfrostbyte.jello.module.settings.impl.ModeSetting;
 import com.mentalfrostbyte.jello.module.settings.impl.NumberSetting;
 import net.minecraft.item.ItemStack;
@@ -15,7 +18,7 @@ import team.sdhq.eventBus.annotations.EventTarget;
 public class FastEat extends Module {
    public FastEat() {
       super(ModuleCategory.PLAYER, "FastEat", "Allows you to eat faster");
-      this.registerSetting(new ModeSetting("Mode", "Mode", 0, "Basic", "Hypixel")
+      this.registerSetting(new ModeSetting("Mode", "Mode", 0, "Basic", "Hypixel", "Timer")
 
       );
       this.registerSetting(new NumberSetting<Float>("Speed", "Eating speed.", 0.55F, Float.class, 0.0F, 1.0F, 0.01F));
@@ -31,6 +34,9 @@ public class FastEat extends Module {
                && (float) mc.player.getItemInUseCount() < this.getNumberValueBySettingName("Speed") * 32.0F) {
                String mode = this.getStringSettingValueByName("Mode");
                switch (mode) {
+                  case "Timer":
+                     Client.getInstance().moduleManager.getModuleByClass(Timer.class).toggle();
+
                   case "Basic":
                      int x = mc.player.getItemInUseCount() + 2;
 
@@ -41,7 +47,7 @@ public class FastEat extends Module {
                      break;
                   case "Hypixel":
                      mc.getConnection()
-                        .sendPacket(new CHeldItemChangePacket(mc.player.inventory.currentItem + 1 >= 9 ? 0 : mc.player.inventory.currentItem + 1));
+                             .sendPacket(new CHeldItemChangePacket(mc.player.inventory.currentItem + 1 >= 9 ? 0 : mc.player.inventory.currentItem + 1));
                      mc.getConnection().sendPacket(new CHeldItemChangePacket(mc.player.inventory.currentItem));
                }
             }
