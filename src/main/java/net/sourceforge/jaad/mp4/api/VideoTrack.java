9bf07e4a8010bd4bfd35c84a374622ff48c3e730
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-02-10 18:26:59 +0200

    Update VideoTrack.java

--- src/main/java/net/sourceforge/jaad/mp4/api/VideoTrack.java
+++ src/main/java/net/sourceforge/jaad/mp4/api/VideoTrack.java
@@ -71,34 +71,14 @@ public class VideoTrack extends Track {
 	}
 
 	public int getWidth() {
-		return (sampleEntry!=null) ? sampleEntry.getWidth() : 0;
+		return sampleEntry.getWidth();
 	}
 
 	public int getHeight() {
-		return (sampleEntry!=null) ? sampleEntry.getHeight() : 0;
-	}
-
-	public double getHorizontalResolution() {
-		return (sampleEntry!=null) ? sampleEntry.getHorizontalResolution() : 0;
-	}
-
-	public double getVerticalResolution() {
-		return (sampleEntry!=null) ? sampleEntry.getVerticalResolution() : 0;
-	}
-
-	public int getFrameCount() {
-		return (sampleEntry!=null) ? sampleEntry.getFrameCount() : 0;
-	}
-
-	public String getCompressorName() {
-		return (sampleEntry!=null) ? sampleEntry.getCompressorName() : "";
+		return sampleEntry.getHeight();
 	}
 
 	public int getDepth() {
-		return (sampleEntry!=null) ? sampleEntry.getDepth() : 0;
-	}
-
-	public int getLayer() {
-		return tkhd.getLayer();
+		return sampleEntry.getDepth();
 	}
 }
