a282142de378c2147c240ec675de6cc1b99c72fb
lamzvam <jazen.lamz@proton.me>
2025-02-11 22:57:42 +0100

    push fixed ItemPhysics

--- src/main/java/com/mentalfrostbyte/jello/module/impl/render/ItemPhysics.java
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/render/ItemPhysics.java
@@ -3,8 +3,17 @@ package com.mentalfrostbyte.jello.module.impl.render;
 import com.mentalfrostbyte.jello.module.Module;
 import com.mentalfrostbyte.jello.module.ModuleCategory;
 
+import com.mentalfrostbyte.jello.module.Module;
+import com.mentalfrostbyte.jello.module.ModuleCategory;
+import com.mentalfrostbyte.jello.module.settings.impl.BooleanSetting;
+import com.mentalfrostbyte.jello.module.settings.impl.NumberSetting;
+
 public class ItemPhysics extends Module {
     public ItemPhysics() {
-        super(ModuleCategory.RENDER, "ItemPhysics", "Better phyisics for items");
+        super(ModuleCategory.RENDER, "ItemPhysics", "Better physics for items");
+        this.registerSetting(new BooleanSetting("Disable Floating", "Disable the item floating on drop.", false));
+        this.registerSetting(new BooleanSetting("Enable Gravity", "The item rotates on the air.", false));
+        this.registerSetting(new NumberSetting<Float>("Gravity Value", "How fast the items rotates on fall.", 1, Float.class, 1, 3, 0.05f));
+        this.registerSetting(new BooleanSetting("Loaf Always", "Always render the items sided.", false));
     }
 }
