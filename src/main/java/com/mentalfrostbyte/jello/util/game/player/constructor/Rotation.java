a4599470e58952d57b53b947ef7ee18049806712
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-02-11 16:39:40 -0600

    feat(Rotation): add `of` method, currently only `Entity`s are supported

--- src/main/java/com/mentalfrostbyte/jello/util/game/player/constructor/Rotation.java
+++ src/main/java/com/mentalfrostbyte/jello/util/game/player/constructor/Rotation.java
@@ -1,5 +1,7 @@
 package com.mentalfrostbyte.jello.util.game.player.constructor;
 
+import net.minecraft.entity.Entity;
+
 public class Rotation {
     public float yaw, pitch;
     public float lastYaw, lastPitch;
@@ -9,5 +11,14 @@ public class Rotation {
         this.lastPitch = pitch;
         this.yaw = yaw;
         this.pitch = pitch;
+    }
+    public Rotation(float yaw, float pitch, float lastYaw, float lastPitch) {
+        this.lastYaw = lastYaw;
+        this.lastPitch = lastPitch;
+        this.yaw = yaw;
+        this.pitch = pitch;
+    }
+    public static Rotation of(Entity entity) {
+        return new Rotation(entity.rotationYaw, entity.rotationPitch, entity.prevRotationYaw, entity.prevRotationPitch);
     }
 }
