5752f92a407eead228ee870ef96b3364de3e8bae
StormingMoon <stormingmoon@proton.me>
2025-02-12 22:47:05 +0100

    Add a AutoJump option to VanillaSpeed  :)
    Vroom Vroom

--- src/main/java/com/mentalfrostbyte/jello/module/impl/movement/speed/VanillaSpeed.java
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/movement/speed/VanillaSpeed.java
@@ -3,6 +3,7 @@ package com.mentalfrostbyte.jello.module.impl.movement.speed;
 import com.mentalfrostbyte.jello.event.impl.player.movement.EventMove;
 import com.mentalfrostbyte.jello.module.Module;
 import com.mentalfrostbyte.jello.module.ModuleCategory;
+import com.mentalfrostbyte.jello.module.settings.impl.BooleanSetting;
 import com.mentalfrostbyte.jello.module.settings.impl.NumberSetting;
 import com.mentalfrostbyte.jello.util.game.player.MovementUtil;
 import team.sdhq.eventBus.annotations.EventTarget;
@@ -10,7 +11,9 @@ import team.sdhq.eventBus.annotations.EventTarget;
 public class VanillaSpeed extends Module {
     public VanillaSpeed() {
         super(ModuleCategory.MOVEMENT, "Vanilla", "Vanilla speed");
+        this.registerSetting(new BooleanSetting("AutoJump","AutoJumps",false));
         this.registerSetting(new NumberSetting("Speed", "Speed value", 4.0F, Float.class, 1.0F, 10.0F, 0.1F));
+
     }
 
     @EventTarget
@@ -18,6 +21,8 @@ public class VanillaSpeed extends Module {
         if (this.isEnabled()) {
             double speedInput = MovementUtil.getSmartSpeed() * (double) this.getNumberValueBySettingName("Speed");
             MovementUtil.setMotion(event, speedInput);
+            if(this.getBooleanValueFromSettingName("AutoJump"))
+                mc.gameSettings.keyBindJump.setPressed(true);
         }
     }
 }
