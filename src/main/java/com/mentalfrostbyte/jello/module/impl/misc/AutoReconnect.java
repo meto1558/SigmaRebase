46f0dc1876609fef4c4319b35929702f268f8d8b
4meav <py96982015@gmail.com>
2025-02-13 18:41:27 -0800

    Fix merge

--- src/main/java/com/mentalfrostbyte/jello/module/impl/misc/AutoReconnect.java
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/misc/AutoReconnect.java
@@ -3,6 +3,7 @@ package com.mentalfrostbyte.jello.module.impl.misc;
 import com.mentalfrostbyte.jello.event.impl.game.world.EventLoadWorld;
 import com.mentalfrostbyte.jello.module.Module;
 import com.mentalfrostbyte.jello.module.ModuleCategory;
+import com.mentalfrostbyte.jello.module.settings.impl.BooleanSetting;
 import net.minecraft.client.multiplayer.ServerData;
 import team.sdhq.eventBus.annotations.EventTarget;
 
@@ -12,6 +13,12 @@ public class AutoReconnect extends Module {
 
     public AutoReconnect() {
         super(ModuleCategory.MISC, "AutoReconnect", "Automatically reconnects you to the server if you got disconnected");
+
+        BooleanSetting oneTime = new BooleanSetting("One time", "Reconnect only once before disabling AutoReconnect", false);
+        this.registerSetting(oneTime);
+
+        BooleanSetting reconnectButton = new BooleanSetting("Reconnect button", "Show reconnect button on the disconnected screen", true);
+        this.registerSetting(reconnectButton);
     }
 
     @EventTarget
