b3b066aa91c35b252cef847641a10ab411b2afef
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-02-16 15:46:37 +0200

    fix client freeze on 1.20.2 and above

--- src/main/java/net/minecraft/client/gui/screen/DownloadTerrainScreen.java
+++ src/main/java/net/minecraft/client/gui/screen/DownloadTerrainScreen.java
@@ -1,35 +1,34 @@
 package net.minecraft.client.gui.screen;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.chat.NarratorChatListener;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.optifine.CustomLoadingScreen;
 import net.optifine.CustomLoadingScreens;
 
-public class DownloadTerrainScreen extends Screen
-{
+public class DownloadTerrainScreen extends Screen {
     private static final ITextComponent field_243307_a = new TranslationTextComponent("multiplayer.downloadingTerrain");
     private CustomLoadingScreen customLoadingScreen = CustomLoadingScreens.getCustomLoadingScreen();
 
-    public DownloadTerrainScreen()
-    {
+    public DownloadTerrainScreen() {
         super(NarratorChatListener.EMPTY);
     }
 
-    public boolean shouldCloseOnEsc()
-    {
+    public boolean shouldCloseOnEsc() {
         return false;
     }
 
-    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta)
-    {
-        if (this.customLoadingScreen != null)
-        {
-            this.customLoadingScreen.drawBackground(this.width, this.height);
+    public void render(MatrixStack matrices, int mouseX, int mouseY, float delta) {
+        if (Minecraft.getInstance().world != null && Minecraft.getInstance().player != null) {
+            Minecraft.getInstance().displayGuiScreen(null);
+            return;
         }
-        else
-        {
+
+        if (this.customLoadingScreen != null) {
+            this.customLoadingScreen.drawBackground(this.width, this.height);
+        } else {
             this.renderDirtBackground(0);
         }
 
@@ -37,8 +36,7 @@ public class DownloadTerrainScreen extends Screen
         super.render(matrices, mouseX, mouseY, delta);
     }
 
-    public boolean isPauseScreen()
-    {
+    public boolean isPauseScreen() {
         return false;
     }
 }
