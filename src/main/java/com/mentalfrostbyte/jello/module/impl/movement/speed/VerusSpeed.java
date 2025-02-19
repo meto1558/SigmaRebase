fa69bd5c741d92147e6539f46a61845b74ba6180
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-02-18 20:11:23 -0600

    feat(VerusSpeed): slipperiness speed boost

--- src/main/java/com/mentalfrostbyte/jello/module/impl/movement/speed/VerusSpeed.java
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/movement/speed/VerusSpeed.java
@@ -6,7 +6,9 @@ import com.mentalfrostbyte.jello.module.ModuleCategory;
 import com.mentalfrostbyte.jello.module.settings.impl.BooleanSetting;
 import com.mentalfrostbyte.jello.module.settings.impl.ModeSetting;
 import com.mentalfrostbyte.jello.util.game.player.MovementUtil;
+import net.minecraft.block.BlockState;
 import net.minecraft.potion.Effects;
+import net.minecraft.util.math.BlockPos;
 import team.sdhq.eventBus.annotations.EventTarget;
 
 /**
@@ -23,6 +25,15 @@ public class VerusSpeed extends Module {
     private double speed = 0;
     private int airTicks = 0;
 
+    public BlockState blockStateUnder() {
+        BlockPos under = new BlockPos(
+                mc.player.getPosX(),
+                mc.player.getBoundingBox().minY - 0.5000001D,
+                mc.player.getPosZ()
+        );
+        return mc.world.getBlockState(under);
+    }
+
     @EventTarget
     public void onMotion(EventUpdateWalkingPlayer event) {
         boolean dmgBoost = getBooleanValueFromSettingName("Damage boost");
@@ -38,9 +49,13 @@ public class VerusSpeed extends Module {
                     } else {
                         speed = 0.377D;
                     }
+
                     if (!mc.player.isSprinting())
                         speed *= 0.78D;
 
+                    float slipperiness = blockStateUnder().getBlock().getSlipperiness();
+                    if (slipperiness != 0.6f)
+                        speed += (slipperiness * 0.36);
                     mc.player.jump();
                 }
 
@@ -65,6 +80,11 @@ public class VerusSpeed extends Module {
                     if (!mc.player.isSprinting())
                         speed *= 0.78D;
 
+                    float slipperiness = blockStateUnder().getBlock().getSlipperiness();
+
+                    if (slipperiness != 0.6f)
+                        speed += (slipperiness * 0.35);
+
                     mc.player.jump();
                 }
 
@@ -80,7 +100,13 @@ public class VerusSpeed extends Module {
 
             case "Ground" -> {
                 if (mc.player.onGround) {
-                    MovementUtil.strafe(0.23 + (Math.random() / 150d));
+                    double speed = 0.23 + (Math.random() / 150d);
+                    float slipperiness = blockStateUnder().getBlock().getSlipperiness();
+
+                    if (slipperiness != 0.6f)
+                        speed += (slipperiness * 0.35);
+
+                    MovementUtil.strafe(speed);
                 }
 
                 if (mc.player.hurtTime > 0 && dmgBoost) {
@@ -100,6 +126,10 @@ public class VerusSpeed extends Module {
                     } else {
                         speed = 0.377D;
                     }
+
+                    float slipperiness = blockStateUnder().getBlock().getSlipperiness();
+                    if (slipperiness != 0.6f)
+                        speed += (slipperiness * 0.37);
 
                     mc.player.jump();
                 }
