fea314f764d7db56e78f5cdb2d9365e6b44066a0
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-02-20 07:26:16 -0600

    fix(VerusNoFall): paste from OldHypixel NoFall and add set motion
    Fixes #22

--- src/main/java/com/mentalfrostbyte/jello/module/impl/player/nofall/VerusNoFall.java
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/player/nofall/VerusNoFall.java
@@ -4,20 +4,35 @@ import com.mentalfrostbyte.jello.event.impl.player.EventPlayerTick;
 import com.mentalfrostbyte.jello.event.impl.player.movement.EventUpdateWalkingPlayer;
 import com.mentalfrostbyte.jello.module.Module;
 import com.mentalfrostbyte.jello.module.ModuleCategory;
+import com.mentalfrostbyte.jello.util.game.world.blocks.BlockUtil;
 import team.sdhq.eventBus.annotations.EventTarget;
 
 public class VerusNoFall extends Module {
+    private double stage;
+
     public VerusNoFall() {
         super(ModuleCategory.PLAYER, "Verus", "Verus NoFall");
     }
+
     @EventTarget
-    public void onTick(EventPlayerTick __) {
+    public void onUpdate(EventUpdateWalkingPlayer event) {
         if (!this.isEnabled()) return;
-        // Thanks, @alarmingly_good.
-        if (!mc.player.onGround && mc.player.getMotion().y < 0 && mc.player.fallDistance > 2) {
-            mc.player.onGround = true;
+        assert mc.player != null;
+        if (mc.player.getPosY() < 2.0) return;
+        if (!event.isPre()) return;
+        if (BlockUtil.isAboveBounds(mc.player, 1.0E-4F)) {
+            this.stage = 0.0;
+            return;
+        }
+
+        if (mc.player.getMotion().y < -0.1) {
+            this.stage = this.stage - mc.player.getMotion().y;
+        }
+
+        if (this.stage > 3.0 && mc.player.fallDistance > 2) {
+            this.stage = 1.0E-14;
             mc.player.setMotion(mc.player.getMotion().x, 0.0, mc.player.getMotion().z);
-            mc.player.fallDistance = 0;
+            event.setOnGround(true);
         }
     }
 
