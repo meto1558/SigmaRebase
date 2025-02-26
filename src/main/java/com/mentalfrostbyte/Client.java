661bfd85edce72e689dea4cedd6b4efb5ff517c6
MarkGG8181 <86848614+MarkGG8181@users.noreply.github.com>
2025-02-26 21:13:04 +0200

    pojav same thing

--- src/main/java/com/mentalfrostbyte/Client.java
+++ src/main/java/com/mentalfrostbyte/Client.java
@@ -36,7 +36,8 @@ import java.util.ConcurrentModificationException;
 import java.util.List;
 
 public class Client {
-
+    //test
+    
     private static final Minecraft mc = Minecraft.getInstance();
     public static int currentVersionIndex = 28;
 
