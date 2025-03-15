2073a9adace1f1bef18d9346b68cdc54376ad73a
MarkGG8181 <86848614+MarkGG8181@users.noreply.github.com>
2025-03-15 16:13:17 +0200

    Revert "set java.awt.headless"
    This reverts commit c8a747b47b79c5db4936fe64e95539380810feb5.

--- src/main/java/com/mentalfrostbyte/Client.java
+++ src/main/java/com/mentalfrostbyte/Client.java
@@ -80,7 +80,6 @@ public class Client {
     public BlurEngine blurEngine;
 
     public void start() {
-        System.setProperty("java.awt.headless", "false");
         this.logger = new ClientLogger(System.out, System.out, System.err);
         this.logger.info("Initializing...");
         CloudConfigs.start();
