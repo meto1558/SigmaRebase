9d27c19544e33169bc73f6c1a1f4a9782aa66fcd
mark <86848614+MarkGG8181@users.noreply.github.com>
2025-03-17 21:55:50 +0200

    <?>

--- src/main/java/com/mentalfrostbyte/jello/module/settings/impl/SubOptionSetting.java
+++ src/main/java/com/mentalfrostbyte/jello/module/settings/impl/SubOptionSetting.java
@@ -6,11 +6,11 @@ import com.mentalfrostbyte.jello.module.settings.SettingType;
 import java.util.List;
 
 public class SubOptionSetting extends SubOptionSetting2 {
-   public SubOptionSetting(String name, String description, boolean defaultValue, List<Setting> subSettings) {
+   public SubOptionSetting(String name, String description, boolean defaultValue, List<Setting<?>> subSettings) {
       super(name, description, SettingType.SUBOPTION, defaultValue, subSettings);
    }
 
-   public SubOptionSetting(String name, String description, boolean defaultValue, Setting... subSettings) {
+   public SubOptionSetting(String name, String description, boolean defaultValue, Setting<?>... subSettings) {
       super(name, description, SettingType.SUBOPTION, defaultValue, subSettings);
    }
 }
