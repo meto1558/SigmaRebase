27e2471c26ce1b0f3aa9d51da8ca3e5f554b18ab
mark <86848614+MarkGG8181@users.noreply.github.com>
2025-03-18 18:22:41 +0200

    Update CorrectMovement.java

--- src/main/java/com/mentalfrostbyte/jello/module/impl/movement/CorrectMovement.java
+++ src/main/java/com/mentalfrostbyte/jello/module/impl/movement/CorrectMovement.java
@@ -24,7 +24,7 @@ public class CorrectMovement extends Module {
     @EventTarget
     @HighestPriority
     public void onJump(EventJump event) {
-        event.yaw = (RotationCore.currentYaw);
+        event.setYaw(RotationCore.currentYaw);
     }
 
     @EventTarget
