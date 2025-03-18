502c2ce762b70a8dea12a6a7c880f8d28d02d41a
mark <86848614+MarkGG8181@users.noreply.github.com>
2025-03-18 18:52:49 +0200

    reocode moveRelative event

--- src/main/java/com/mentalfrostbyte/jello/event/impl/player/movement/EventMoveFlying.java
+++ src/main/java/com/mentalfrostbyte/jello/event/impl/player/movement/EventMoveFlying.java
@@ -1,47 +1,15 @@
 package com.mentalfrostbyte.jello.event.impl.player.movement;
 
-import team.sdhq.eventBus.Event;
+import com.mentalfrostbyte.jello.event.CancellableEvent;
 
-public class EventMoveFlying extends Event {
-    private float yaw;
-    private double strafe, forward, friction;
+public class EventMoveFlying extends CancellableEvent {
+    public float yaw;
+    public float strafe, forward, friction;
 
-    public EventMoveFlying(float yaw, double strafe, double forward, double friction) {
+    public EventMoveFlying(float yaw, float strafe, float forward, float friction) {
         this.yaw = yaw;
         this.strafe = strafe;
         this.forward = forward;
-        this.friction = friction;
-    }
-
-    public float getYaw() {
-        return yaw;
-    }
-
-    public void setYaw(float yaw) {
-        this.yaw = yaw;
-    }
-
-    public double getStrafe() {
-        return strafe;
-    }
-
-    public void setStrafe(double strafe) {
-        this.strafe = strafe;
-    }
-
-    public double getForward() {
-        return forward;
-    }
-
-    public void setForward(double forward) {
-        this.forward = forward;
-    }
-
-    public double getFriction() {
-        return friction;
-    }
-
-    public void setFriction(double friction) {
         this.friction = friction;
     }
 }
