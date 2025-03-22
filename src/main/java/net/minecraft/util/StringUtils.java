0eb6f0e379b35aa164aa13c20d093045eb99e254
mark <86848614+MarkGG8181@users.noreply.github.com>
2025-03-22 16:22:13 +0200

    StringUtils wrapText

--- src/main/java/net/minecraft/util/StringUtils.java
+++ src/main/java/net/minecraft/util/StringUtils.java
@@ -1,5 +1,9 @@
 package net.minecraft.util;
 
+import org.newdawn.slick.TrueTypeFont;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Pattern;
 import javax.annotation.Nullable;
 
@@ -29,5 +33,61 @@ public class StringUtils
     public static boolean isNullOrEmpty(@Nullable String string)
     {
         return org.apache.commons.lang3.StringUtils.isEmpty(string);
+    }
+
+    public static String[] wrapText(String text, int maxWidth, TrueTypeFont font) {
+        String[] words = text.split(" ");
+        Map<Integer, String> lines = new HashMap<>();
+        int lineIndex = 0;
+
+        for (String word : words) {
+            String currentLine = lines.getOrDefault(lineIndex, "");
+            boolean isNewLine = !lines.containsKey(lineIndex);
+            boolean fitsInLine = font.getWidth(currentLine) + font.getWidth(word) <= maxWidth;
+            boolean wordExceedsWidth = font.getWidth(word) >= maxWidth;
+
+            if (!fitsInLine && !wordExceedsWidth) {
+                lineIndex++;
+                currentLine = lines.getOrDefault(lineIndex, "");
+                isNewLine = !lines.containsKey(lineIndex);
+                fitsInLine = font.getWidth(currentLine) + font.getWidth(word) <= maxWidth;
+                wordExceedsWidth = font.getWidth(word) >= maxWidth;
+            }
+
+            if (fitsInLine) {
+                lines.put(lineIndex, isNewLine ? word : currentLine + " " + word);
+            } else if (wordExceedsWidth) {
+                while (wordExceedsWidth && !fitsInLine) {
+                    int trimIndex = 0;
+
+                    while (true) {
+                        if (trimIndex <= word.length()) {
+                            String trimmedWord = word.substring(0, word.length() - trimIndex);
+                            if (font.getWidth(trimmedWord) > maxWidth) {
+                                trimIndex++;
+                                continue;
+                            }
+
+                            lines.put(++lineIndex, trimmedWord);
+                            word = word.substring(word.length() - trimIndex);
+                        }
+
+                        currentLine = lines.getOrDefault(lineIndex, "");
+                        fitsInLine = font.getWidth(currentLine) + font.getWidth(word) <= maxWidth;
+                        wordExceedsWidth = font.getWidth(word) >= maxWidth;
+                        isNewLine = !lines.containsKey(lineIndex);
+                        break;
+                    }
+                }
+
+                if (!fitsInLine) {
+                    lineIndex++;
+                }
+
+                lines.put(lineIndex, word);
+            }
+        }
+
+        return lines.values().toArray(new String[0]);
     }
 }
