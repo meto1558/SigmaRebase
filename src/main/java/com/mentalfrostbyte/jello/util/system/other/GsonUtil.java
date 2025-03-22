4762b751e24f94465d17c3a746b149def1714dbf
mark <86848614+MarkGG8181@users.noreply.github.com>
2025-03-22 18:37:47 +0200

    null checks

--- src/main/java/com/mentalfrostbyte/jello/util/system/other/GsonUtil.java
+++ src/main/java/com/mentalfrostbyte/jello/util/system/other/GsonUtil.java
@@ -1,37 +1,70 @@
 package com.mentalfrostbyte.jello.util.system.other;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
 public class GsonUtil {
     public static boolean getBooleanOrDefault(JsonObject jsonObject, String key, boolean defaultValue) {
+        JsonElement jsonElement;
+
         try {
-            return jsonObject.get(key).getAsBoolean();
+            jsonElement = jsonObject.get(key);
+
+            if (jsonElement == null) {
+                return defaultValue;
+            }
+
+            return jsonElement.getAsBoolean();
         } catch (JsonParseException e) {
             return defaultValue;
         }
     }
 
     public static int getIntOrDefault(JsonObject jsonObject, String key, int defaultValue) {
+        JsonElement jsonElement;
+
         try {
-            return jsonObject.get(key).getAsInt();
+            jsonElement = jsonObject.get(key);
+
+            if (jsonElement == null) {
+                return defaultValue;
+            }
+
+            return jsonElement.getAsInt();
         } catch (JsonParseException e) {
             return defaultValue;
         }
     }
 
     public static float getFloatOrDefault(JsonObject jsonObject, String key, float defaultValue) {
+        JsonElement jsonElement;
+
         try {
-            return (float) jsonObject.get(key).getAsDouble();
+            jsonElement = jsonObject.get(key);
+
+            if (jsonElement == null) {
+                return defaultValue;
+            }
+
+            return (float) jsonElement.getAsDouble();
         } catch (JsonParseException e) {
             return defaultValue;
         }
     }
 
     public static String getStringOrDefault(JsonObject jsonObject, String key, String defaultValue) {
+        JsonElement jsonElement;
+
         try {
-            return jsonObject.get(key).getAsString();
+            jsonElement = jsonObject.get(key);
+
+            if (jsonElement == null) {
+                return defaultValue;
+            }
+
+            return jsonElement.getAsString();
         } catch (JsonParseException e) {
             return defaultValue;
         }
