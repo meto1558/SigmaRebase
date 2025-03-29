7700752eacdb358070182fec61b331702b4f7625
DataM0del <183248792+DataM0del@users.noreply.github.com>
2025-03-28 23:23:24 -0500

    refactor: replace Timer with TimerUtil

--- src/main/java/com/mentalfrostbyte/jello/managers/AnimationManager.java
+++ src/main/java/com/mentalfrostbyte/jello/managers/AnimationManager.java
@@ -1,24 +1,24 @@
 package com.mentalfrostbyte.jello.managers;
 
 import com.mentalfrostbyte.jello.util.system.math.RandomIntGenerator;
-import com.mentalfrostbyte.jello.util.system.math.counter.Timer;
+import com.mentalfrostbyte.jello.util.system.math.counter.TimerUtil;
 
 public class AnimationManager {
    private float currentValue;
    private final RandomIntGenerator random = new RandomIntGenerator();
-   private final Timer fieldTimer = new Timer();
+   private final TimerUtil timer = new TimerUtil();
    private long nextInterval;
    private boolean isAnimating = false;
    private float targetValue = -1.0F;
 
    public AnimationManager() {
-      this.fieldTimer.start();
+      this.timer.start();
       this.nextInterval = this.random.nextInt(8000, 10000);
       this.currentValue = this.random.nextFloat();
    }
 
    public void update() {
-      if (this.fieldTimer.getElapsedTime() > this.nextInterval) {
+      if (this.timer.getElapsedTime() > this.nextInterval) {
          this.nextInterval = this.random.nextInt(8000, 10000);
          this.isAnimating = true;
          this.targetValue = this.random.nextFloat() + 0.75F;
@@ -27,10 +27,10 @@ public class AnimationManager {
             this.targetValue *= -1.0F;
          }
 
-         this.fieldTimer.reset();
+         this.timer.reset();
       }
 
-      if (this.isAnimating && this.targetValue != -1.0F && this.fieldTimer.getElapsedTime() % 10L == 0L) {
+      if (this.isAnimating && this.targetValue != -1.0F && this.timer.getElapsedTime() % 10L == 0L) {
          if (!(this.targetValue > this.currentValue)) {
             this.currentValue -= 0.02F;
             if (this.targetValue > this.currentValue) {
